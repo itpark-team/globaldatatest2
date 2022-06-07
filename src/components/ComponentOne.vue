7456daae5958d65b7551b862179f4a0007b68f50
itpark <a.m.smyslov@yandex.ru>
2022-06-07 21:53:13 +0300

    done change between two components

---
+++ src/components/ComponentOne.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button v-on:click="emitEvent" >Emit Event</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentOne",
+  methods: {
+    emitEvent() {
+      this.eventBus.emit('test', 'Welcome to MNP!')
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ComponentTwo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "ComponentTwo",
+  data() {
+    return {
+      message: "111"
+    }
+  },
+  mounted() {
+    this.eventBus.on('test', (args) => {
+      this.message = args;
+    })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
